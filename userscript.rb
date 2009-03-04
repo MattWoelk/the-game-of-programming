803160526904732b0a07106eb9db9bc259c475b0
Matthew Woelk <spike_of_winnipeg@hotmail.com>
2009-03-04 13:53:10 -0600

    minor changes

--- userscript.rb
+++ userscript.rb
@@ -23,43 +23,52 @@ def input!
 end
 
 def iterate
+  puts "iterating."
   @x = @xp
   @y = @yp
 end
 
 def wait_for_ack
-  while (inn = input!.length) <= @inputted.length 
+  while (inn = input!).length <= @inputted.length
   end
   puts "#{@x},#{@y}"
-  iterate if (inn <=> "bad") != 0
+  iterate if (inn[-1].strip <=> "bad") != 0
   
-  puts "ACK!" if (inn <=> "ack") != 0
-  puts "bad" if (inn <=> "bad") != 0
+  puts "ACK!" if (inn[-1].strip <=> "ack") == 0
+  puts "bad" if (inn[-1].strip <=> "bad") == 0
   go_to_grass @x, @y
   @inputted = input!
 end
 
 def tall_grass? x, y
-  return true if in?(x, y) && (@data[x][y] <=> 'W') == 0
-  false
+  puts "tall grass at #{x}, #{y}?"
+  puts "in: #{in?(x, y)}"
+  in?(x, y) && (@data[x][y] <=> 'W') == 0
 end
 
 def in? x, y
-  return true if x >= 0 && y >= 0 && x < @data.length && y < @data[0].length
+  puts "@data.length: #{@data.length}"
+  puts "@data[0].length: #{@data[0].length}"
+  x >= 0 && y >= 0 && x < @data.length && y < @data[0].length
 end
 
 def go_to_grass x, y
+  puts "trying to go to grass"
   if tall_grass? x-1, y
     @xp = x-1
+    puts "w"
     output! "\nw"
   elsif tall_grass? x, y-1
     @yp = y-1
+    puts "n"
     output! "\nn"
   elsif tall_grass? x+1, y
     @xp = x+1
+    puts "e"
     output! "\ne"
   elsif tall_grass? x, y+1
     @yp = y+1
+    puts "s"
     output! "\ns"
   else
     go_at_random
@@ -100,14 +109,21 @@ if __FILE__ == $0
   @xp = 0
   @yp = 0
   
-  @data = Array.new(@inputted.length)
-  @data.length.times do |x|
-    @data[x] = []
-    @inputted[0].length.times do |y|
-      @data[x] << @inputted[x][y,1]
-    end
+  @data = @inputted.map{|e| e.strip.split}
+  @new = []
+  c = 0
+  while @data[c] != [] && !@data[c].nil?
+    @new << @data[c]
+    c += 1
   end
-  go_at_random
+  @data = @new
+  #this is now a 2D array of our data.
+  p @data
+  
+  #go_at_random
+  @inputted = @inputted[0]
+  #p @inputted
+  #p @inputted.length
   while (@inputted[-1] <=> "victory!") != 0
     wait_for_ack
   end
